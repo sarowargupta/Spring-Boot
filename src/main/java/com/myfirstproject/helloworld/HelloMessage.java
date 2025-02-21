0fb768fa6189957df1a25a5769846ae8f97b5511
sarowargupta <sarowargupta2001@gmail.com>
2025-02-21 15:13:20 +0530

    [Sarowar Gupta]add. Code for hello messaging app by RequestParam query.

--- src/main/java/com/myfirstproject/helloworld/HelloMessage.java
+++ src/main/java/com/myfirstproject/helloworld/HelloMessage.java
@@ -1,5 +1,6 @@
 package com.myfirstproject.helloworld;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 //UC_1_ShowHelloMessage
@@ -8,5 +9,11 @@ public class HelloMessage {
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello from BridgeLabz";
+    }
+
+//UC_2_ShowHelloName as query parameter
+    @GetMapping("/query")
+    public String sayHelloWithName(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz";
     }
 }
